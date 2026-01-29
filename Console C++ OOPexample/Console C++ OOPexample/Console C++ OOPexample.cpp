95045114b152e980ad566596c43b6e848176d626
Nikita <nikita.gaber2703@gmail.com>
2026-01-29 14:15:08 +0200

    Add files via upload

---
+++ Console C++ OOPexample/Console C++ OOPexample/Console C++ OOPexample.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node {
+    int value;
+	Node* prev;
+	Node* next;
+
+	Node(int value) {
+		this->value = value;
+		this->prev = nullptr;
+		this->next = nullptr;
+	}
+};
+
+int main()
+{
+	Node* first = new Node(1);
+	Node* second = new Node(2);
+
+	first->next = second;
+
+	Node* third = new Node(3);
+	second->next = third;
+
+	Node* four = new Node(4);
+	third->next = four;
+
+	Node* newNode = new Node(99);
+
+	
+	newNode->next = nullptr;
+	newNode->prev = four;
+	four->next = newNode;
+	four = newNode;
+
+	Node* current = first;
+	while (current != nullptr) {
+		cout << current->value << " ";
+		current = current->next;
+	}
+
+}
