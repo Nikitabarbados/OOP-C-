aeb6ddcdabdf4d2080c8a0d7e0f9fabd020584a4
Nikita <nikita.gaber2703@gmail.com>
2026-01-29 14:22:19 +0200

    Add files via upload

---
+++ Console С++ OOPPraktika/Console С++ OOPPraktika/Console С++ OOPPraktika.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+__interface ButyStudentom {
+    void Study();
+    void Sleep();
+    void Party();
+};
+
+class Nikita : public ButyStudentom {
+public:
+    void Study() {
+        cout << "Никита зубрить конспект перед екзаменом..." << "\n";
+    }
+    void Sleep() {
+        cout << "Никита спить 4 години після пар..." << "\n";
+    }
+    void Party() {
+        cout << "Никита тусить з друзями всю ніч!" << "\n";
+    }
+};
+
+int main() {
+    setlocale(0, "UKR");
+
+    Nikita st;
+    st.Study();
+    st.Sleep();
+    st.Party();
+}
