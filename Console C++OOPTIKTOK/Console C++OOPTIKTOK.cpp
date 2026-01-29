81eb1ba1d532a0d812ef34aad3b08c9247713bb3
Nikita <nikita.gaber2703@gmail.com>
2026-01-29 14:27:07 +0200

    Add files via upload

---
+++ Console C++OOPTIKTOK/Console C++OOPTIKTOK.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>	
+using namespace std;
+
+class Base {
+public:
+	void display() {
+		cout << "Base class" << "\n";
+	}
+};
+class Delivered : public Base {
+public:
+	void display() {
+		cout << "Derived class" << "\n";
+	}
+};
+
+int main() {
+	Base* ptr = new Delivered();
+	ptr->display();
+	delete ptr;
+}
